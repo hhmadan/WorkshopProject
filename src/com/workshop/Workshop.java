d4c3169517f10bb36572cdf13437842707100895
hhmadan <hhmadan03@gmail.com>
2022-10-18 12:12:00 +0530

    Display Welcome message on master branch

---
+++ src/com/workshop/Workshop.java
@@ -0,0 +1,9 @@
+//Display Welcome message on master branch
+
+package com.workshop;
+public class Workshop {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome in Workshop Project...!");
+    }
+}
